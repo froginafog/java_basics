e952d88739b067f22d48a531c9a0d083cc13d5f9
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-12 17:23:53 -0500

    Add files via upload

---
+++ 0012/main.java
@@ -0,0 +1,39 @@
+public class Main
+{
+    public static void main(String[] args)  
+	{
+		int i = 0;
+		
+		System.out.println("increasing the value of i:");
+		
+		while(i <= 3)  //while i is less than or equal to 3
+		{
+			System.out.println("i: " + i);
+			i++;  //increase the value of i by 1
+			      //i = i + 1 
+		}
+		
+		System.out.println("decreasing the value of i:");
+		
+		do
+		{
+			System.out.println("i: " + i);
+			i--;  //decrease the value of i by 1
+			      //i = i - 1 
+		}while(i >= 0);
+	}
+}
+
+/*
+increasing the value of i:
+i: 0
+i: 1
+i: 2
+i: 3
+decreasing the value of i:
+i: 4
+i: 3
+i: 2
+i: 1
+i: 0
+*/
