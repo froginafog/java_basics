3bd9da92bd1738c12f19740e7f35f2980fc70252
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-12 17:22:55 -0500

    Add files via upload

---
+++ 0011/main.java
@@ -0,0 +1,42 @@
+public class Main
+{
+    public static void main(String[] args)  
+	{
+		int a = 10;
+		int b = 5; 
+		int c = 15;
+		
+		//|| stands for OR
+		
+		//true OR true is true
+		//true OR false is true
+		//false OR true is true
+		//false OR false is false
+		
+		if(a < b || a < c)
+		{
+			System.out.println("a is smaller than b or a smaller than c");
+		}
+		
+		if(a > b || a < c )
+		{
+			System.out.println("a is greater than b or a is smaller than c");
+		}
+		
+		if(a > b || a > c)
+		{
+			System.out.println("a is greater than b or a is greater than c");
+		}
+		
+		if(a < b || a > c)
+		{
+			System.out.println("a is smaller than b or a is greater than c");
+		}
+	}
+}
+
+/*
+a is smaller than b or a smaller than c
+a is greater than b or a is smaller than c
+a is greater than b or a is greater than c
+*/
