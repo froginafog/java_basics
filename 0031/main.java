a47b24f3d2143eaabad7562995650d919c2dc3b3
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-14 19:03:00 -0500

    Update main.java

--- 0031/main.java
+++ 0031/main.java
@@ -19,7 +19,7 @@ public class Main
 		System.out.println("inside function_3");
 	}
 	
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{
 		System.out.println("calling function_1");
 		function_1();
