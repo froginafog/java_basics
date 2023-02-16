1a8a776fb81822fb9b67b65dbdbb1d6173af97bd
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-15 22:19:58 -0500

    Add files via upload

--- 0033/main.java
+++ 0033/main.java
@@ -1,34 +1,34 @@
 //only one "public" class is allowed per file
 public class Main
 {
-    	public static void main(String[] args)  
+    public static void main(String[] args)  
 	{
-		Game main_object_1 = new Game();
-		Game main_object_2 = new Game();
+		Game game_object_1 = new Game();
+		Game game_object_2 = new Game();
 		
-		main_object_1.game_name = "Game One";
-		main_object_1.num_players = 2;
-		main_object_1.player_names[0] = "Player One In Game One";
-		main_object_1.player_names[1] = "Player Two In Game One";
+		game_object_1.game_name = "Game One";
+		game_object_1.num_players = 2;
+		game_object_1.player_names[0] = "Player One In Game One";
+		game_object_1.player_names[1] = "Player Two In Game One";
 		
-		main_object_2.game_name = "Game Two";
-		main_object_2.num_players = 4;
-		main_object_2.player_names[0] = "Player One In Game Two";
-		main_object_2.player_names[1] = "Player Two In Game Two";
-		main_object_2.player_names[2] = "Player Three In Game Two";
-		main_object_2.player_names[3] = "Player Four In Game Two";
+		game_object_2.game_name = "Game Two";
+		game_object_2.num_players = 4;
+		game_object_2.player_names[0] = "Player One In Game Two";
+		game_object_2.player_names[1] = "Player Two In Game Two";
+		game_object_2.player_names[2] = "Player Three In Game Two";
+		game_object_2.player_names[3] = "Player Four In Game Two";
 		
-		System.out.println("main_object_1.game_name      : " + main_object_1.game_name);
-		System.out.println("main_object_1.num_players    : " + main_object_1.num_players);
-		System.out.println("main_object_1.player_names[0]: " + main_object_1.player_names[0]);
-		System.out.println("main_object_1.player_names[1]: " + main_object_1.player_names[1]);
+		System.out.println("game_object_1.game_name      : " + game_object_1.game_name);
+		System.out.println("game_object_1.num_players    : " + game_object_1.num_players);
+		System.out.println("game_object_1.player_names[0]: " + game_object_1.player_names[0]);
+		System.out.println("game_object_1.player_names[1]: " + game_object_1.player_names[1]);
 		System.out.println("------------------------------------------------------------");
-		System.out.println("main_object_2.game_name      : " + main_object_2.game_name);
-		System.out.println("main_object_2.num_players    : " + main_object_2.num_players);
-		System.out.println("main_object_2.player_names[0]: " + main_object_2.player_names[0]);
-		System.out.println("main_object_2.player_names[1]: " + main_object_2.player_names[1]);
-		System.out.println("main_object_2.player_names[2]: " + main_object_2.player_names[2]);
-		System.out.println("main_object_2.player_names[3]: " + main_object_2.player_names[3]);
+		System.out.println("game_object_2.game_name      : " + game_object_2.game_name);
+		System.out.println("game_object_2.num_players    : " + game_object_2.num_players);
+		System.out.println("game_object_2.player_names[0]: " + game_object_2.player_names[0]);
+		System.out.println("game_object_2.player_names[1]: " + game_object_2.player_names[1]);
+		System.out.println("game_object_2.player_names[2]: " + game_object_2.player_names[2]);
+		System.out.println("game_object_2.player_names[3]: " + game_object_2.player_names[3]);
 	}
 }
 
@@ -43,15 +43,15 @@ class Game
 }
 
 /*
-main_object_1.game_name      : Game One
-main_object_1.num_players    : 2
-main_object_1.player_names[0]: Player One In Game One
-main_object_1.player_names[1]: Player Two In Game One
+game_object_1.game_name      : Game One
+game_object_1.num_players    : 2
+game_object_1.player_names[0]: Player One In Game One
+game_object_1.player_names[1]: Player Two In Game One
 ------------------------------------------------------------
-main_object_2.game_name      : Game Two
-main_object_2.num_players    : 4
-main_object_2.player_names[0]: Player One In Game Two
-main_object_2.player_names[1]: Player Two In Game Two
-main_object_2.player_names[2]: Player Three In Game Two
-main_object_2.player_names[3]: Player Four In Game Two
+game_object_2.game_name      : Game Two
+game_object_2.num_players    : 4
+game_object_2.player_names[0]: Player One In Game Two
+game_object_2.player_names[1]: Player Two In Game Two
+game_object_2.player_names[2]: Player Three In Game Two
+game_object_2.player_names[3]: Player Four In Game Two
 */
