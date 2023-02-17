efe301a3650ae755105e9f3c145f6f91e0ed65d7
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-16 20:09:49 -0500

    Update main.java

--- 0021/main.java
+++ 0021/main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{
 		int num_items = 3;
 		int[] array_of_integers = new int[num_items];  
