cc66b4a992e4c260ce55d19b9a0ec71b3a602048
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-16 22:00:02 -0500

    Add files via upload

---
+++ 0038/main.java
@@ -0,0 +1,27 @@
+public class Main
+{
+	String game_name;
+	
+	public Main(String game_name)  //constructor
+	{
+		System.out.println("A constructor for the class called \"Main\" is created.");
+		this.game_name = game_name;
+	}
+	//a constructor is called whenever an object of the class is created
+
+    public static void main(String[] args)  
+	{
+		Main main_object_1 = new Main("Game One");
+		Main main_object_2 = new Main("Game Two");
+		
+		System.out.println("main_object_1.game_name: " + main_object_1.game_name);
+		System.out.println("main_object_2.game_name: " + main_object_2.game_name);
+	}
+}
+
+/*
+A constructor for the class called "Main" is created.
+A constructor for the class called "Main" is created.
+main_object_1.game_name: Game One
+main_object_2.game_name: Game Two
+*/
