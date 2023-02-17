1eadc65dfddce0eadae79795e5379f4667938e47
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-16 20:10:23 -0500

    Update main.java

--- 0022/main.java
+++ 0022/main.java
@@ -1,12 +1,12 @@
 public class Main
 {
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{
 		int matrix[][] = {
-							{11, 12, 13, 14, 15},
-							{21, 22, 23, 24, 25},
-							{31, 32, 33, 34, 35}
-						 }; 
+					{11, 12, 13, 14, 15},
+					{21, 22, 23, 24, 25},
+					{31, 32, 33, 34, 35}
+				 }; 
 						 
 		//print the items of the matrix
 		for(int i = 0; i < 3; i++)  //loop through the rows of the matrix
