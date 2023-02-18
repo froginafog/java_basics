9aa40d5faa7d3bdaa2f22d5d1e524289af795f5c
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-18 00:48:53 -0500

    Add files via upload

---
+++ 0039/main.java
@@ -0,0 +1,73 @@
+public class Main
+{
+    public static void main(String[] args)  
+	{	
+		String[] array_of_player_names_1 = {"Player One In Game One", 
+		                                    "Player Two In Game One"};                                  
+		String[] array_of_player_names_2 = {"Player One In Game Two",
+											"Player Two In Game Two",
+											"Player Three In Game Two",
+											"Player Four In Game Two"};									
+		Game game_object_1 = new Game("Game One", 2, array_of_player_names_1);
+		Game game_object_2 = new Game("Game Two", 4, array_of_player_names_2);
+							
+		
+		System.out.println("Game Name        : " + game_object_1.game_name);
+		System.out.println("Number of Players: " + game_object_1.num_players);
+		System.out.println("Player Names     :");
+		for(int i = 0; i < game_object_1.player_names.length; i++)
+		{
+			if(game_object_1.player_names[i] != null)
+			{
+				System.out.println(game_object_1.player_names[i]);
+			}
+		}
+		
+		System.out.println("-----------------------------------");
+		
+		System.out.println("Game Name        : " + game_object_2.game_name);
+		System.out.println("Number of Players: " + game_object_2.num_players);
+		System.out.println("Player Names     :");
+		for(int i = 0; i < game_object_2.player_names.length; i++)
+		{
+			if(game_object_2.player_names[i] != null)
+			{
+				System.out.println(game_object_2.player_names[i]);
+			}
+		}
+	}
+}
+
+class Game  
+{
+	int MAX_NUM_PLAYERS = 4;
+	String game_name;
+	int num_players;
+	String[] player_names = new String[MAX_NUM_PLAYERS]; 
+	
+	public Game(String game_name, int num_players, String[] player_names)  //constructor
+	{
+		this.game_name = game_name;
+		this.num_players = num_players;
+		for(int i = 0; i < player_names.length && i < MAX_NUM_PLAYERS; i++)
+		{
+			this.player_names[i] = player_names[i];
+		}
+	}
+}
+
+/*
+Game Name        : Game One
+Number of Players: 2
+Player Names     :
+Player One In Game One
+Player Two In Game One
+-----------------------------------
+Game Name        : Game Two
+Number of Players: 4
+Player Names     :
+Player One In Game Two
+Player Two In Game Two
+Player Three In Game Two
+Player Four In Game Two
+*/
---
+++ 0040/main.java
@@ -0,0 +1,98 @@
+public class Main
+{
+    public static void main(String[] args)  
+	{	
+		String[] array_of_player_names = {"Player One", 
+		                                  "Player Two",
+		                                  "Player Three",
+		                                  "Player Four"};                                  
+		
+		Game game_object = new Game();
+		
+		game_object.add_player_name("Player One");
+		for(int i = 0; i < game_object.player_names.length; i++)
+		{
+			System.out.println(game_object.player_names[i]);
+		}
+		
+		System.out.println("--------------1--------------");
+		
+		game_object.add_player_name("Player Two");
+		for(int i = 0; i < game_object.player_names.length; i++)
+		{
+			System.out.println(game_object.player_names[i]);
+		}
+		
+		System.out.println("--------------2--------------");
+		
+		game_object.add_player_name("Player Three");
+		for(int i = 0; i < game_object.player_names.length; i++)
+		{
+			System.out.println(game_object.player_names[i]);
+		}
+		
+		System.out.println("--------------3--------------");
+		
+		game_object.add_player_name("Player Four");
+		for(int i = 0; i < game_object.player_names.length; i++)
+		{
+			System.out.println(game_object.player_names[i]);
+		}
+		
+		System.out.println("--------------4--------------");
+		
+		game_object.add_player_name("Player Five");
+
+		System.out.println("--------------5--------------");
+	}
+}
+
+class Game  
+{
+	int MAX_NUM_PLAYERS = 4;
+	String[] player_names = new String[MAX_NUM_PLAYERS]; 
+	int index_for_player_name;
+	
+	Game()
+	{
+		index_for_player_name = -1;
+	}
+	
+	void add_player_name(String player_name)
+	{
+		if(index_for_player_name >= MAX_NUM_PLAYERS - 1)
+		{
+			System.out.println("array of player_names is full");
+		}
+		else
+		{
+			index_for_player_name++;
+			player_names[index_for_player_name] = player_name;
+		}
+	}
+}
+
+/*
+Player One
+null
+null
+null
+--------------1--------------
+Player One
+Player Two
+null
+null
+--------------2--------------
+Player One
+Player Two
+Player Three
+null
+--------------3--------------
+Player One
+Player Two
+Player Three
+Player Four
+--------------4--------------
+array of player_names is full
+--------------5--------------
+*/
