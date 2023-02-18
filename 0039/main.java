ab38492095cd58ad20ae299a4722eaa2205035b6
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-18 00:49:10 -0500

    Update main.java

--- 0039/main.java
+++ 0039/main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{	
 		String[] array_of_player_names_1 = {"Player One In Game One", 
 		                                    "Player Two In Game One"};                                  
