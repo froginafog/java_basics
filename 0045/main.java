19bd38ed621a199ade18c75f49b771b5d6a23a9f
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-19 18:37:34 -0500

    Update main.java

--- 0045/main.java
+++ 0045/main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{	                 
 		Game game_object = new Game("Game One");
 		Game new_game_object = new Game();
