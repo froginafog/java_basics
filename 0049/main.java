189a20e9617538218741369b8458576494645489
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-22 18:44:32 -0500

    Update main.java

--- 0049/main.java
+++ 0049/main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{	             
 		Player player_object = new Player();
 		//creating an object of a derived class also invokes the constructor of the base class
