56727cf7e1f7f987f6b366b300f465e5444ad05e
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-22 18:44:18 -0500

    Add files via upload

---
+++ 0049/main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+    public static void main(String[] args)  
+	{	             
+		Player player_object = new Player();
+		//creating an object of a derived class also invokes the constructor of the base class
+		//the constructor of the base class is called first
+	}
+}
+
+class Game  //base class (or superclass)
+{
+	Game()
+	{
+		System.out.println("constructor of the \"Game\" class is called");
+	}
+	
+}
+
+class Player extends Game   
+{
+	Player()
+	{
+		System.out.println("constructor of the \"Player\" class is called");
+	}
+}
+
+/*
+constructor of the "Game" class is called
+constructor of the "Player" class is called
+*/
---
+++ 0050/main.java
@@ -0,0 +1,43 @@
+public class Main
+{
+    public static void main(String[] args)  
+	{	             
+		Player player_object = new Player("Game One", "Player One");
+		//creating an object of a derived class also invokes the constructor of the base class
+		//the constructor of the base class is called first
+		
+		System.out.println("player_object.game_name  : " + player_object.game_name);
+		System.out.println("player_object.player_name: " + player_object.player_name);
+	}
+}
+
+class Game  //base class (or superclass)
+{
+	String game_name;
+
+	Game(String game_name)
+	{
+		System.out.println("constructor of the \"Game\" class is called");
+		this.game_name = game_name;
+	}
+	
+}
+
+class Player extends Game   
+{
+	String player_name;
+
+	Player(String game_name, String player_name)
+	{
+		super(game_name);  //call the constructor of the base class (it must be on the first line of this constructor)
+		System.out.println("constructor of the \"Player\" class is called");
+		this.player_name = player_name;
+	}
+}
+
+/*
+constructor of the "Game" class is called
+constructor of the "Player" class is called
+player_object.game_name  : Game One
+player_object.player_name: Player One
+*/
