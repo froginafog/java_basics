61bbd406d0bb157538c61b144dd4763582374719
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-24 21:48:06 -0500

    Add files via upload

---
+++ 0052/main.java
@@ -0,0 +1,99 @@
+public class Main
+{
+    public static void main(String[] args)  
+	{	             
+		Status status_object = new Status("Initial Game Name", "Initial Player Name", "Initial Status Name");
+		
+		System.out.println("status_object.game_name  : " + status_object.game_name);
+		System.out.println("status_object.player_name: " + status_object.player_name);
+		System.out.println("status_object.status_name: " + status_object.status_name);
+		
+		System.out.println("----------------------------------------------------");
+		
+		status_object.set_game_name("Game One");
+		status_object.set_player_name("Player One");
+		status_object.set_status_name("Alive");
+		
+		System.out.println("status_object.get_game_name()  : " + status_object.get_game_name());
+		System.out.println("status_object.get_player_name(): " + status_object.get_player_name());
+		System.out.println("status_object.get_status_name(): " + status_object.get_status_name());
+	}
+}
+
+class Game  //base class (or superclass)
+{
+	String game_name;
+
+	Game(String game_name)
+	{
+		System.out.println("constructor of the \"Game\" class is called");
+		this.game_name = game_name;
+	}
+	
+	void set_game_name(String game_name)
+	{
+		this.game_name = game_name;
+	}
+	
+	String get_game_name()
+	{
+		return game_name;
+	}
+}
+
+class Player extends Game   
+{
+	String player_name;
+
+	Player(String game_name, String player_name)
+	{
+		super(game_name);  
+		System.out.println("constructor of the \"Player\" class is called");
+		this.player_name = player_name;
+	}
+	
+	void set_player_name(String player_name)
+	{
+		this.player_name = player_name;
+	}
+	
+	String get_player_name()
+	{
+		return player_name;
+	}
+}
+
+class Status extends Player
+{
+	String status_name;
+	
+	Status(String game_name, String player_name, String status_name)
+	{
+		super(game_name, player_name); 
+		System.out.println("constructor of the \"Status\" class is called");
+		this.status_name = status_name;
+	}
+	
+	void set_status_name(String status_name)
+	{
+		this.status_name = status_name;
+	}
+	
+	String get_status_name()
+	{
+		return status_name;
+	}
+}
+
+/*
+constructor of the "Game" class is called
+constructor of the "Player" class is called
+constructor of the "Status" class is called
+status_object.game_name  : Initial Game Name
+status_object.player_name: Initial Player Name
+status_object.status_name: Initial Status Name
+----------------------------------------------------
+status_object.get_game_name()  : Game One
+status_object.get_player_name(): Player One
+status_object.get_status_name(): Alive
+*/
