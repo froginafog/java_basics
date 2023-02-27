68cea20866376cde53855e7a2833dacbc23b4011
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-27 17:56:10 -0500

    Update Main.java

--- 0065/Main.java
+++ 0065/Main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{	             
 		String string_1 = "Python";
 		String string_2 = "";
