7da620932a643cf33b32ecb91dcebf2186ac2fd6
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-27 14:44:33 -0500

    Update Main.java

--- 0058/Main.java
+++ 0058/Main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public static void main(String[] args)  
+    	public static void main(String[] args)  
 	{	             
 		Game.game_price = 89.99;
 		System.out.println("Game.game_price: " + Game.game_price);
