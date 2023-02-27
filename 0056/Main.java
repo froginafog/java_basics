4876d2c57af5f769b42f02576056fc2600edd34c
Liang D.S <80227533+froginafog@users.noreply.github.com>
2023-02-26 22:50:12 -0500

    Add files via upload

--- 0056/Main.java
+++ 0056/Main.java
@@ -4,42 +4,55 @@
 //The methods in an "interface" are implicitly abstract (we don't have to specify them).
 //The methods in an "interface" are implicitly public (we don't have to specify them).
 //No constructors in an "interface".
-//All variables defined in an "interface" must "static" and "final"
+//All variables defined in an "interface" are "static" and "final" by default.
+//A "static" variable can be accessed by the class name without the need to use an object to access it.
+//A "final" variable is a variable whose value cannot be changed.
 //If a class implements an "interface", all of the methods in the "interface" must be defined in the class (except if the class is "abstract").
 //A class inherits from an "interface".
 
 public class Main
 {
-    	public static void main(String[] args)  
+    public static void main(String[] args)  
 	{	             
 		Game game_object = new Game();
 		
 		game_object.game_name = "Game One";
 		game_object.print_welcome_message();
 		game_object.print_goodbye_message();
+		System.out.
+		System.out.println("Message.default_welcome_message: " + Message.default_welcome_message);
 	}
 }
 
 interface Message  //"abstract" by default
 {
-	String default_welcome_message = "Welcome";  //must be assigned a value
-	String default_goodbye_message = "Thank you for playing";  //must be assigned a value
-	void print_welcome_message();  //"public" and "abstract" by default  
-	void print_goodbye_message();  //"public" and "abstract" by default  
+	String default_welcome_message = "Welcome";  
+	//it is "static" and "final" by default
+	//must be assigned a value
+	
+	String default_goodbye_message = "Thank you for playing";  
+	//it is "static" and "final" by default
+	//must be assigned a value
+	
+	void print_welcome_message();  
+	//"public" and "abstract" by default 
+	 
+	void print_goodbye_message(); 
+	//"public" and "abstract" by default  
 }
 
 class Game implements Message
 {
 	String game_name;
 	
-    	public void print_welcome_message()  
-    	{
+    public void print_welcome_message()  
+    {
 		System.out.println(default_welcome_message + " to " + game_name);
 	}
 	//This method must be public because it is part of an "interface".
 	
 	public void print_goodbye_message()
-    	{
+    {
 		System.out.println(default_goodbye_message + " " + game_name);
 	}
 	//This method must be public because it is part of an "interface".
